65ee3192787d9d5b653b00492fce907aa9517953
Shane88 <4056111+Shane88@users.noreply.github.com>
2014-05-06 19:35:24 +1200

    Remove Nongeneric IsNotEmpty overload which allows List specific overloads to be removed.

--- EnsureArg.Tests/IEnsureArgCollectionExtensions_Tests.cs
+++ EnsureArg.Tests/IEnsureArgCollectionExtensions_Tests.cs
@@ -1,7 +1,6 @@
 ﻿namespace EnsureArg.Tests
 {
    using System;
-   using System.Collections;
    using System.Collections.Generic;
    using FluentAssertions;
    using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -42,51 +41,6 @@
       {
          // Arrange.
          List<object> objects = new List<object>()
-         {
-            null
-         };
-
-         // Act.
-         Action action = () =>
-            Ensure.Arg(objects).IsNotEmpty();
-
-         // Assert.
-         action.ShouldNotThrow();
-      }
-
-      [TestMethod]
-      public void When_IsNotEmpty_is_called_with_a_null_List()
-      {
-         // Arrange.
-         ArrayList objects = null;
-
-         // Act.
-         Action action = () =>
-            Ensure.Arg(objects).IsNotEmpty();
-
-         // Assert.
-         action.ShouldThrow<ArgumentNullException>();
-      }
-
-      [TestMethod]
-      public void When_IsNotEmpty_is_called_with_a_empty_List()
-      {
-         // Arrange.
-         ArrayList objects = new ArrayList();
-
-         // Act.
-         Action action = () =>
-            Ensure.Arg(objects).IsNotEmpty();
-
-         // Assert.
-         action.ShouldThrow<ArgumentException>();
-      }
-
-      [TestMethod]
-      public void When_IsNotEmpty_is_called_with_a_valid_List()
-      {
-         // Arrange.
-         ArrayList objects = new ArrayList()
          {
             null
          };
@@ -190,51 +144,6 @@
       }
 
       [TestMethod]
-      public void When_IsNotEmpty_is_called_with_a_null_ICollection()
-      {
-         // Arrange.
-         ICollection objects = null;
-
-         // Act.
-         Action action = () =>
-            Ensure.Arg(objects).IsNotEmpty();
-
-         // Assert.
-         action.ShouldThrow<ArgumentNullException>();
-      }
-
-      [TestMethod]
-      public void When_IsNotEmpty_is_called_with_a_empty_ICollection()
-      {
-         // Arrange.
-         ICollection objects = new List<object>();
-
-         // Act.
-         Action action = () =>
-            Ensure.Arg(objects).IsNotEmpty();
-
-         // Assert.
-         action.ShouldThrow<ArgumentException>();
-      }
-
-      [TestMethod]
-      public void When_IsNotEmpty_is_called_with_a_valid_ICollection()
-      {
-         // Arrange.
-         ICollection objects = new List<object>()
-         {
-            null
-         };
-
-         // Act.
-         Action action = () =>
-            Ensure.Arg(objects).IsNotEmpty();
-
-         // Assert.
-         action.ShouldNotThrow();
-      }
-
-      [TestMethod]
       public void When_IsNotEmpty_is_called_with_a_null_array()
       {
          // Arrange.
@@ -277,6 +186,20 @@
 
          // Assert.
          action.ShouldNotThrow();
+      }
+
+      [TestMethod]
+      public void When_IsNotEmpty_is_called_with_a_null_Dictionary()
+      {
+         // Arrange.
+         Dictionary<object, object> objects = null;
+
+         // Act.
+         Action action = () =>
+            Ensure.Arg(objects).IsNotEmpty();
+
+         // Assert.
+         action.ShouldThrow<ArgumentNullException>();
       }
    }
 }
--- EnsureArg/IEnsureArgCollectionExtensions.cs
+++ EnsureArg/IEnsureArgCollectionExtensions.cs
@@ -5,36 +5,6 @@
 
    public static class IEnsureArgCollectionExtensions
    {
-      public static IEnsureArg<List<T>> IsNotEmpty<T>(
-         this IEnsureArg<List<T>> ensureArg,
-         string exceptionMessage = null,
-         params object[] formatArgs)
-      {
-         ensureArg.IsNotNull(exceptionMessage, formatArgs);
-
-         if (ensureArg.Value.Count <= 0)
-         {
-            ensureArg.ThrowArgumentException(exceptionMessage, formatArgs);
-         }
-
-         return ensureArg;
-      }
-
-      public static IEnsureArg<IList<T>> IsNotEmpty<T>(
-         this IEnsureArg<IList<T>> ensureArg,
-         string exceptionMessage = null,
-         params object[] formatArgs)
-      {
-         ensureArg.IsNotNull(exceptionMessage, formatArgs);
-
-         if (ensureArg.Value.Count <= 0)
-         {
-            ensureArg.ThrowArgumentException(exceptionMessage, formatArgs);
-         }
-
-         return ensureArg;
-      }
-
       public static IEnsureArg<ICollection<T>> IsNotEmpty<T>(
          this IEnsureArg<ICollection<T>> ensureArg,
          string exceptionMessage = null,
@@ -46,22 +16,7 @@
          {
             ensureArg.ThrowArgumentException(exceptionMessage, formatArgs);
          }
-
-         return ensureArg;
-      }
-
-      public static IEnsureArg<ICollection> IsNotEmpty(
-         this IEnsureArg<ICollection> ensureArg,
-         string exceptionMessage = null,
-         params object[] formatArgs)
-      {
-         ensureArg.IsNotNull(exceptionMessage, formatArgs);
-
-         if (ensureArg.Value.Count <= 0)
-         {
-            ensureArg.ThrowArgumentException(exceptionMessage, formatArgs);
-         }
-
+         
          return ensureArg;
       }
 
