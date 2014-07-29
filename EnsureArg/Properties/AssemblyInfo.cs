bf3311284b7e81906257e8bd7854fbf9b92ae7d3
Shane88 <4056111+Shane88@users.noreply.github.com>
2014-07-30 07:21:28 +1200

    Turned on Xml documentation for EnsureArg and EnsureArg.BclExtension release builds. Bumped version numbers to 1.0.1 to match new nuget package version that will be created to distribute the intellisense files. Updated release notes in nuspec file.

--- EnsureArg/Properties/AssemblyInfo.cs
+++ EnsureArg/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ See the project site for many more examples")]
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 [assembly: CLSCompliant(true)]
