80ed512728b1013dd570ea8f3f8f00970733a5e3
jcfarsight <jcfarsight@localhost>
2015-02-16 18:35:08 +1300

    Minor tweaks and enhancements to EnsureArg library. - Updated NuGet dependencies to latest versions. - Set SmartFormat.NET SpecificVersion to false to fix issue #1. - Double quote encapsulated post-build step to support path whitespace. - Added IsNotEmptyGuid extension as per issue #2.

---
+++ EnsureArg/Extensions/IEnsureArgGuidExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace EnsureArg
+{
+   using System;
+   using EnsureArg.Core;
+
+   /// <summary>
+   /// Contains IEnsureArg extension methods for performing checks on <see
+   /// cref="System.Guid" /> values.
+   /// </summary>
+   public static class IEnsureArgGuidExtensions
+   {
+      /// <summary>
+      /// Checks whether the value contained in the IEnsureArg instance is a non-empty Guid.
+      /// </summary>
+      /// <param name="ensureArg">
+      /// The IEnsureArg instance, usually created from an Ensure.Arg() call.
+      /// </param>
+      /// <param name="exceptionMessage">
+      /// Optional exception message to use if the null check fails. This exception message will
+      /// override the message supplied in the Ensure.Arg() call, if any.
+      /// </param>
+      /// <returns>The ensureArg instance.</returns>
+      /// <exception cref="System.ArgumentNullException">ensureArg.Value is null.</exception>
+      /// <exception cref="System.ArgumentOutOfRangeException">
+      /// ensureArg.Value is greater than other.
+      /// </exception>
+      public static IEnsureArg<Guid> IsNotEmptyGuid(
+         this IEnsureArg<Guid> ensureArg,
+         string exceptionMessage = null)
+      {
+         ensureArg.ValidateEnsureArgIsNotNull();
+
+         if (ensureArg.Value.Equals(Guid.Empty))
+         {
+            ensureArg.ThrowArgumentOutOfRangeException(Guid.Empty, exceptionMessage);
+         }
+
+         return ensureArg;
+      }
+   }
+}
